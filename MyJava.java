4dc06e05a270f5ab6c764b95ee546837f1919d56
ange <119826331+villange@users.noreply.github.com>
2022-12-15 20:08:19 +0800

    Update MyJava.java

--- MyJava.java
+++ MyJava.java
@@ -1,14 +1,17 @@
 public class MyJava
 {
-  public static void main(String[] args)
+  public static void printName(String name, int age, String course)
   {
     // Insert code from line below this comment where it instantiates the MyJava object into variable named pin, then call the method named printName(String name);
     // Expected Line 1, replace this comment with code on pull request
     // Expected Line 2, replace this comment with code on pull request
+     System.out.println("Hello! My name is " + name + "!" + "I am" + age + "years old" + "I am partaking " + course + "as my course");
   }
-  
-  public void printName(String name)
-  {
-    System.out.println("Hello! My name is " + name + "!");
+  public static void main(String[] args) {
+    String name = "Clarisse Andal";
+   String course = "BSIT";
+   int age = 21;
+
+ printName(name, age, course);
   }
 }
