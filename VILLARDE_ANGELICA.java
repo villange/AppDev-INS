3898e81f5c2b116f7d14e6078410e1767a56de3c
ange <119826331+villange@users.noreply.github.com>
2022-12-15 11:16:19 +0800

    Update and rename MyJava.java to VILLARDE_ANGELICA.java

---
+++ VILLARDE_ANGELICA.java
@@ -0,0 +1,48 @@
+public class TestFactory{
+  public static void main (String[]args){
+    Laptop min = LaptopFactory.getSpecs("min",8,256,"i5-12450Hz");
+    Laptop reco = LaptopFactory.getSpecs("reco", 16,512,"17-12700Hz");
+    
+    System.out.println("minimum specs:" + min);
+    System.out.println("recommended specs:" + reco);
+  }
+}
+abstract class Laptop{
+  public abstract int getRAM();
+  public abstract int getSSD();
+  public abstract int getCPU();
+  
+  public String toString(){
+    return "RAM="+this.getRAM()+"GB, SSD="+this.getSSD()+", CPU="+this 
+      .getCPU();
+  }
+}
+class Minimum extends Laptop{
+  private int ram;
+    private int ssd;
+    private String cpu;
+
+  public int getRAM(){
+    return this.ram;
+  }
+  public int getSSD(){
+    return this.ssd;
+  }
+   public String getCPU(){
+    return this.cpu;
+  }
+  public Recommended(int ram, int ssd, String cpu){
+    this.ram=ram;
+    this.ssd=ssd;
+    this.cpu=cpu;
+  }
+}
+class LaptopFactory{
+  public static Laptop getSpecs(String type, int ram, int ssd, String cpu){
+    if("min".equalsIgnoreCase(type))
+      return new Minimum(ram,ssd,cpu);
+    else if("reco".equalsIgnoreCase(type))
+      return new Recommended (ram,ssd,cpu);
+    return null;
+  }
+}
